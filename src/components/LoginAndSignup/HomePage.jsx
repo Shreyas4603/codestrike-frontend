de04355403d4370f611cda272b4ad8b3f28ac898
Pgsaisuhas <saisuhas.1324@gmail.com>
2024-10-08 22:41:24 +0530

    tested logout functionality

--- src/components/LoginAndSignup/HomePage.jsx
+++ src/components/LoginAndSignup/HomePage.jsx
@@ -1,7 +1,29 @@
 import React from "react";
-
+import Cookies from "js-cookie";
+import { toast } from "sonner";
+import { useNavigate } from "react-router-dom";
 const HomePage = () => {
-  return <div>HomePage</div>;
+  
+
+  const navigate = useNavigate();
+  const handleLogout = () => {
+    Cookies.remove("token", { path: "/" }); // Specify the path to ensure removal
+    toast.success("Logged out successfully!");
+    navigate("/login");
+  };
+
+  const token = Cookies.get("token");
+
+  return (
+    <div>
+      HomePage
+      {token && (
+        <button onClick={handleLogout} className="text-white-600 font-bold hover:underline">
+          Logout
+        </button>
+      )}
+    </div>
+  );
 };
 
 export default HomePage;
